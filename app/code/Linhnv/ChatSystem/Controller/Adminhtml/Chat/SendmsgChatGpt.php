32430f056d1dd83f7a07dd02d8881bc9fdfd457d
linhnv <linhnv@mageplaza.com>
2023-05-23 11:32:18 +0700

    fix bug.

--- app/code/Linhnv/ChatSystem/Controller/Adminhtml/Chat/SendmsgChatGpt.php
+++ app/code/Linhnv/ChatSystem/Controller/Adminhtml/Chat/SendmsgChatGpt.php
@@ -124,16 +124,20 @@ class SendmsgChatGpt extends Action
      */
     public function execute()
     {
+        $token = $this->_helper->getConfig('chat_gpt/access_token');
         $data = $this->_request->getPostValue();
-        $chatGPT = new ChatGPTV2('eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiJsaW5obnZAbWFnZXBsYXphLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlfSwiaHR0cHM6Ly9hcGkub3BlbmFpLmNvbS9hdXRoIjp7InVzZXJfaWQiOiJ1c2VyLXY2MHFFV3NNTW5hc2lHeGxhMWl4ODhETyJ9LCJpc3MiOiJodHRwczovL2F1dGgwLm9wZW5haS5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMTQzMjczNTMxODU4MTM3MTI0NzYiLCJhdWQiOlsiaHR0cHM6Ly9hcGkub3BlbmFpLmNvbS92MSIsImh0dHBzOi8vb3BlbmFpLm9wZW5haS5hdXRoMGFwcC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjgxMTIyMDUxLCJleHAiOjE2ODIzMzE2NTEsImF6cCI6IlRkSkljYmUxNldvVEh0Tjk1bnl5d2g1RTR5T282SXRHIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCBtb2RlbC5yZWFkIG1vZGVsLnJlcXVlc3Qgb3JnYW5pemF0aW9uLnJlYWQgb2ZmbGluZV9hY2Nlc3MifQ.sOkRGauU_oku--IGG2nAecTkbFO97No89HT6j25RBrdOrafBAuo-Pyr1pfP6X27lL1neYGem76t3U7uXeFi4VnHKBsW23sAyx2L7iltE8abcl2xfWNfrhmAsFIAWGF4hh9qR0tVLB4k-XPjWeOyrntUbnvXsYpycQsFyKIossejIkuff0EjomRaz4q1LM8HRap0Zi2rugnz7rHRWT7S6QAb4j3rDec0wjt0gP9zQebVzyMs3Fkh1b1L1r8v3WL_rlj_pt_FZH7-k-XIvLOKGDcuNmT_IC1g6g3vzBDC5iLvHXLRVC3cbkQTS7oMxoKdJ0QEl3kP1kOt6g7pgn1WJqg');
-        $answers = $chatGPT->ask('Hello, how are you?', null, true);
-//        $response = $this->resultFactory->create(ResultFactory::TYPE_JSON);
-//        $response->setHeader('Content-type', 'text/plain');
-//        $response = $response->setData([
-//                'answer' => $answer['answer']
-//            ]);
-//
-//        return $response;
+        $chatGPT = new ChatGPTV2($token);
+        $answers = $chatGPT->ask($data['body_msg']);
+//        foreach ($answers as $item){
+//            $answer = $item;
+//        }
+        $response = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+        $response->setHeader('Content-type', 'text/plain');
+        $response = $response->setData([
+            'answer' => $answers['answer']
+        ]);
+
+        return $response;
 
 
     }
